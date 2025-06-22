21a3cfea9e0e7750d1b32902ccd0a1a8b98b0205
Firas <firas.d@abceez.digital>
2025-06-22 14:44:20 +0100

    feat(AllProjects): Implement project skeleton and fetch all projects on portfolio screen

--- src/components/portfolio/allProjects/AllProjects.tsx
+++ src/components/portfolio/allProjects/AllProjects.tsx
@@ -1,32 +1,63 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 // STORES
 import { useNavbar } from "@/stores/navbar/useNavbar";
+import { useSk_AllProjects } from "@/stores/skeletons/Portfolio/sk_AllProjects";
+
+// SKELETONS
+import { Skeleton_AllProjects } from "@/skeletons";
+
+// APIs
+import { GET_allProjects } from "@/backend/services/portfolio/GET_allProjects";
+
+// TYPES
+import type { Models } from "node_modules/appwrite/types/client";
 
 // ICONS
-import { Monitor, Smartphone } from "lucide-react";
+import { Monitor, MoreHorizontal, Smartphone } from "lucide-react";
 
 export default function AllProjects() {
     const { setLeft, setTitle } = useNavbar();
+    const { sk_AllProjects, setSk_AllProjects } = useSk_AllProjects();
 
+    const [projects, setProjects] = useState<Models.Document[]>([]);
 
     useEffect(() => {
         window.scrollTo({
             top: 0,
             left: 0,
         })
+
+        setSk_AllProjects(true)
+
+        GET_allProjects()
+            .then((res) => {
+                setProjects(res);
+                setSk_AllProjects(false);
+            })
+            .catch((err) => {
+                console.error("Error fetching projects:", err);
+            })
+
     }, [])
+
+    useEffect(() => {
+        console.log(projects)
+
+    }, [projects])
+
+    if (sk_AllProjects || projects.length < 0) return (<Skeleton_AllProjects />)
 
     return (
         <div className="mt-5 flex flex-col space-y-3">
-            {[...Array(10)].map((_, i) => (
+            {projects.map((project) => (
                 <Link
-                    key={i}
-                    to={`/portfolio/${i}`}
+                    key={project.$id}
+                    to={`/portfolio/${project.$id}`}
                     onClick={() => {
                         setLeft("portfolio");
-                        setTitle(`${i}`);
+                        setTitle(`${project.title}`);
                     }}
                     className="flex flex-col md:flex-row rounded-md overflow-hidden shadow-md border-1 bg-white">
 
@@ -37,21 +68,31 @@ export default function AllProjects() {
                             Case study available
                         </div>
 
-                        <div className="text-orange-700 shadow bg-white mt-3 ml-3 px-3 rounded-full py-2 w-fit">
-                            <Smartphone />
-                        </div>
+                        {project.features.includes("Mobile_App") && (
+                            <div className="text-orange-700 shadow bg-white mt-3 ml-3 px-3 rounded-full py-2 w-fit">
+                                <Smartphone />
+                            </div>
+                        )}
 
-                        <div className="text-blue-700 shadow bg-white mt-3 ml-3 px-3 rounded-full py-2 w-fit">
-                            <Monitor />
-                        </div>
+                        {project.features.includes("Web_App") && (
+                            <div className="text-blue-700 shadow bg-white mt-3 ml-3 px-3 rounded-full py-2 w-fit">
+                                <Monitor />
+                            </div>
+                        )}
+
+                        {project.features.length > 0 && (
+                            <div className="text-gray-500 shadow bg-white mt-3 ml-3 px-3 rounded-full py-2 w-fit">
+                                <MoreHorizontal />
+                            </div>
+                        )}
 
                     </div>
 
-                    {/* PRODUCT IMAGE - covers majority */}
+                    {/* PROJECT IMAGE  */}
                     <div
                         className="w-full md:w-2/3 h-[200px] md:h-auto bg-gray-200"
                         style={{
-                            backgroundImage: `url('https://placehold.co/600x400')`,
+                            backgroundImage: `url(${project.coverURL})`,
                             backgroundSize: "cover",
                             backgroundPosition: "center",
                         }}
@@ -61,10 +102,10 @@ export default function AllProjects() {
                     <div className="flex flex-col justify-between p-4 w-full md:w-1/3">
                         <div>
                             <h2 className="text-lg font-semibold line-clamp-2 mb-1">
-                                Product {i} – Modern Tech Gadget
+                                {project.title}
                             </h2>
                             <p className="text-sm text-gray-600 line-clamp-2">
-                                Sleek, powerful, and designed for everyday use. Perfect for modern lifestyles.
+                                {project.overview}
                             </p>
                         </div>
                     </div>
--- src/skeletons/index.ts
+++ src/skeletons/index.ts
@@ -1,3 +1,4 @@
 export { default as Skeleton_Hero } from './HomeScreen/Skeleton_Hero';
 export { default as Skeleton_Article } from './ArticleScreen/Skeleton_Article';
 export { default as Skeleton_AllArticles } from './AllArticles/Skeleton_AllArticles';
+export { default as Skeleton_AllProjects } from './Portfolio/Skeleton_AllProjects';
