d327bc7ab17de8552c4fcff2df2e6c60f3045ce7
Firas <firas.d@abceez.digital>
2025-06-25 13:10:04 +0100

    feat: Set a document title for each page

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,6 +1,9 @@
 import { Auth } from "@/components";
 
 export default function AuthPage() {
+
+  document.title = 'ABCeez Dash | Welcome'
+
   return (
     <>
       <Auth />
--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,9 @@
 import { Error } from "@/components";
 
 export default function ErrorPage() {
+
+      document.title = 'ABCeez Dash | Page Not Found!'
+
   return (
     <Error />
   )
